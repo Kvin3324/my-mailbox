82958a17b9acf02fe2ed367cbff48334528a338f
Kévin Joya <kevin.joya@outlook.com>
2019-05-13 11:36:31 +0200

    Refacto code and install gem

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 
 gem 'devise'
+gem 'devise-bootstrapped', github: 'king601/devise-bootstrapped'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -10,7 +10,7 @@ class ConversationsController < ApplicationController
 
   def create
     if Conversation.between(params[:sender_id], params[:recipient_id]).present?
-      @conversation = Conversation.between(params[:sender_id], params[:recipient_id])
+      @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
     else
       @conversation = Conversation.create!(conversation_params)
     end
